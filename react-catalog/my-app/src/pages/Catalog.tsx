7424f397d87058f4390f78abe07e281d2ee76468
Han-Zhang-Code <zyh514312@gmail.com>
2025-02-20 21:46:37 +0000

    error fixed

--- react-catalog/my-app/src/pages/Catalog.tsx
+++ react-catalog/my-app/src/pages/Catalog.tsx
@@ -4,20 +4,43 @@ import { readCatalog, Product, toDollars } from '../lib';
 
 export default function Catalog() {
   const [products, setProducts] = useState<Product[]>([]);
-
+  const [error, setError] = useState<unknown>();
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
-    readCatalog().then(setProducts);
+    async function load() {
+      try {
+        const products = await readCatalog();
+        setProducts(products);
+      } catch (err) {
+        console.error(err);
+        setError(err);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    setIsLoading(true);
+    load();
   }, []);
 
+  if (isLoading) return <div>Loading...</div>;
+  if (error) {
+    return (
+      <div>
+        Error Loading Catalog
+        {error instanceof Error ? error.message : 'unknown error'}
+      </div>
+    );
+  }
   return (
     <div className="p-4">
       <h1 className="text-4xl font-bold mb-4">Catalog</h1>
       <div className="grid grid-cols-3 gap-4">
         {products.map((product) => (
           <Link
+            key={product.productId}
             to={`/details/${product.productId}`}
             className="text-black-500 mt-2 inline-block">
-            <div key={product.productId} className="border p-4">
+            <div className="border p-4">
               <img
                 src={product.imageUrl}
                 alt={product.name}
@@ -26,7 +49,7 @@ export default function Catalog() {
               <h2 className="text-lg font-bold mt-2">{product.name}</h2>
               <p className="text-gray-600">{toDollars(product.price)}</p>
               <p className="text-sm">{product.shortDescription}</p>
-            </div>{' '}
+            </div>
           </Link>
         ))}
       </div>
